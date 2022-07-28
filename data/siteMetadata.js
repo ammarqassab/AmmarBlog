a75fd3c4a8f24e104c89703b206684d8bd2a4cc7
ammarqassab <ammarqassab97@gmail.com>
2022-07-28 14:43:34 +0300

    Final update

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -5,7 +5,7 @@ const siteMetadata = {
   description: 'A blog created with Next.js and Tailwind.css',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-blog-pi.vercel.app/',
+  siteUrl: 'https://ammar-blog-alpha.vercel.app/',
   siteRepo: '',
   siteLogo: '/static/images/logo.png',
   image: '/static/images/avatar.png',
