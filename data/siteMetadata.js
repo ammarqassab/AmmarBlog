b4341f6bed27ba28255dd9b892324a2ef796f94c
ammarqassab <ammarqassab97@gmail.com>
2022-07-28 14:49:22 +0300

    Final update

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -6,7 +6,7 @@ const siteMetadata = {
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://ammar-blog-alpha.vercel.app/',
-  siteRepo: '',
+  siteRepo: 'https://github.com',
   siteLogo: '/static/images/logo.png',
   image: '/static/images/avatar.png',
   socialBanner: '/static/images/twitter-card.png',
